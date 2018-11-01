f118e679b698160ae19a96b6766c164fe330963c
antHogis <antonjhoglund@hotmail.com>
2018-11-01 23:15:21 +0200

    Add class MainWindow

--- src/main/java/com/github/anthogis/shoppinglist/App.java
+++ src/main/java/com/github/anthogis/shoppinglist/App.java
@@ -4,10 +4,8 @@ package com.github.anthogis.shoppinglist;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
