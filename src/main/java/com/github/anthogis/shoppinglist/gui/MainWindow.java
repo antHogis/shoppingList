c1084f5701a7780a977ad18cffec98f1cceae194
antHogis <antonjhoglund@hotmail.com>
2018-11-01 23:24:07 +0200

    Update MainWindow inheritance
    Update MainWindow to extend javafx.application.Application

---
+++ src/main/java/com/github/anthogis/shoppinglist/gui/MainWindow.java
@@ -0,0 +1,12 @@
+package com.github.anthogis.shoppinglist.gui;
+
+import com.github.anthogis.shoppinglist.gui.MainWindow;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage stage) {
+        
+    }
+}
