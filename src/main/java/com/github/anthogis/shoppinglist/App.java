c9f5c9c493dad99832caceafe2f347bb2b796131
antHogis <antonjhoglund@hotmail.com>
2018-11-03 15:10:36 +0200

    Update class MainWindow
    Update MainWindow functionality, so that it opens a window.
    Add method call to class App, to open the MainWindow

--- src/main/java/com/github/anthogis/shoppinglist/App.java
+++ src/main/java/com/github/anthogis/shoppinglist/App.java
@@ -1,11 +1,14 @@
 package com.github.anthogis.shoppinglist;
 
+import com.github.anthogis.shoppinglist.gui.MainWindow;
+import javafx.application.Application;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        Application.launch(MainWindow.class, args);
     }
 }
--- src/main/java/com/github/anthogis/shoppinglist/gui/MainWindow.java
+++ src/main/java/com/github/anthogis/shoppinglist/gui/MainWindow.java
@@ -1,12 +1,24 @@
 package com.github.anthogis.shoppinglist.gui;
 
-import com.github.anthogis.shoppinglist.gui.MainWindow;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainWindow extends Application {
+    private static final String WINDOW_TITLE = "Shopping List";
+
     @Override
     public void start(Stage stage) {
-        
+        BorderPane bPane = new BorderPane();
+        stage.setTitle(WINDOW_TITLE);
+        stage.initStyle(StageStyle.DECORATED);
+
+        stage.setScene(new Scene(bPane, 640, 480));
+        stage.setResizable(false);
+        stage.show();
+
     }
 }
