97b68939c08aba8c9d64b698049514b965218fdd
antHogis <antonjhoglund@hotmail.com>
2018-11-03 16:25:05 +0200

    Update class MainWindow to use FXML
    - Move mainwindow.fxml to resources folder
    - Update method start in class MainWindow to utilize fxml file

--- src/main/java/com/github/anthogis/shoppinglist/gui/MainWindow.java
+++ src/main/java/com/github/anthogis/shoppinglist/gui/MainWindow.java
@@ -2,21 +2,25 @@ package com.github.anthogis.shoppinglist.gui;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class MainWindow extends Application {
     private static final String WINDOW_TITLE = "Shopping List";
 
     @Override
-    public void start(Stage stage) {
-        BorderPane bPane = new BorderPane();
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
+
         stage.setTitle(WINDOW_TITLE);
         stage.initStyle(StageStyle.DECORATED);
 
-        stage.setScene(new Scene(bPane, 640, 480));
+        stage.setScene(new Scene(root, 640, 480));
         stage.setResizable(false);
         stage.show();
 
