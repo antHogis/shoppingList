6b8af3a283676ac61571599c4ad0ffd93a83d71d
antHogis <antonjhoglund@hotmail.com>
2018-11-03 20:29:52 +0200

    Update class ShoppingListItem
    - Add attributes, constructor, getters and setters for ShoppingListItem

--- src/main/java/com/github/anthogis/shoppinglist/ShoppingListItem.java
+++ src/main/java/com/github/anthogis/shoppinglist/ShoppingListItem.java
@@ -1,4 +1,27 @@
 package com.github.anthogis.shoppinglist;
 
 public class ShoppingListItem {
+    private String itemName;
+    private int itemAmount;
+
+    public ShoppingListItem(String itemName, int itemAmount) {
+        this.itemName = itemName;
+        this.itemAmount = itemAmount;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemAmount() {
+        return itemAmount;
+    }
+
+    public void setItemAmount(int itemAmount) {
+        this.itemAmount = itemAmount;
+    }
 }
