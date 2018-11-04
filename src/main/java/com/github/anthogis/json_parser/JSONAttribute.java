963b43e5dae684351d2981e4609aa3eb6af9e5ee
antHogis <antonjhoglund@hotmail.com>
2018-11-04 12:53:37 +0200

    Prepare JSONAttribute for testing
    Implement a test for formatting the attribute for JSON, for testing that
    generic values and inheritance work as intended

--- src/main/java/com/github/anthogis/json_parser/JSONAttribute.java
+++ src/main/java/com/github/anthogis/json_parser/JSONAttribute.java
@@ -1,4 +1,28 @@
 package com.github.anthogis.json_parser;
 
-public class JSONAttribute {
+import java.util.Collection;
+
+public class JSONAttribute<T> {
+    private String keyWord;
+    private T value;
+    private String notation;
+
+    public JSONAttribute(String keyWord, T value) {
+        this.keyWord = keyWord;
+        this.value = value;
+        formatAttribute();
+    }
+
+    public void formatAttribute() {
+        //notation = '\"' + keyWord + "\": ";
+
+        if (value instanceof Collection) {
+            notation = "array";
+        } else if (value instanceof Number || value instanceof Boolean || value == null) {
+            notation = "no quotes";
+        } else {
+            notation = "as string";
+        }
+    }
+
 }
