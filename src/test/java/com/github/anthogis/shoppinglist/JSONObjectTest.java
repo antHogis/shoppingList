da90352e296ab81b2b6d7a3bf889628d85861c5a
antHogis <antonjhoglund@hotmail.com>
2018-11-15 14:00:56 +0200

    Rename AppTest -> JSONObjectTest

---
+++ src/test/java/com/github/anthogis/shoppinglist/JSONObjectTest.java
@@ -0,0 +1,21 @@
+package com.github.anthogis.shoppinglist;
+
+import com.github.anthogis.json_parser.JSONAttribute;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class JSONObjectTest {
+    @BeforeClass
+    public static void init() {
+
+    }
+}
