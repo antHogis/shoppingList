1402704ab9f098adefc56c8c7f6000f1075a6ac6
antHogis <antonjhoglund@hotmail.com>
2018-11-15 12:55:13 +0200

    Add attributes to class JSONObject
    Add attributes with getters and setters to class JSONObject

--- src/main/java/com/github/anthogis/json_parser/JSONObject.java
+++ src/main/java/com/github/anthogis/json_parser/JSONObject.java
@@ -1,4 +1,27 @@
 package com.github.anthogis.json_parser;
 
+import java.util.List;
+
 public class JSONObject {
+    private String keyWord;
+    private List<JSONAttribute> attributes;
+
+    public JSONObject() {
+    }
+
+    public JSONObject(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    public List<JSONAttribute> getAttributes() {
+        return attributes;
+    }
 }
