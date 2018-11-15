4455931a04be59e65cab266c8043edf34e1e84cb
antHogis <antonjhoglund@hotmail.com>
2018-11-15 14:22:04 +0200

    Add rudimentary test to JSONObjectTest

--- src/test/java/com/github/anthogis/shoppinglist/JSONObjectTest.java
+++ src/test/java/com/github/anthogis/shoppinglist/JSONObjectTest.java
@@ -4,6 +4,7 @@ import com.github.anthogis.json_parser.JSONAttribute;
 
 import static org.junit.Assert.*;
 
+import com.github.anthogis.json_parser.JSONObject;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,8 +15,19 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class JSONObjectTest {
+    static JSONObject testObjectOne;
+
     @BeforeClass
     public static void init() {
+        testObjectOne = new JSONObject();
+        testObjectOne.getAttributes().add(new JSONAttribute<Integer>("fsds", 12));
+        testObjectOne.getAttributes().add(new JSONAttribute<Boolean>("very gay", true));
+        testObjectOne.formatObject();
+    }
 
+    @Test
+    public void testNotation() {
+        System.out.println(testObjectOne.getNotation());
+        assertTrue(testObjectOne.getNotation().equals("{\"fsds\" : 12,\"very gay\" : true}"));
     }
 }
