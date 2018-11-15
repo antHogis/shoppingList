9255ab740b7671227ed7031d81e0e88967bf7ee4
antHogis <antonjhoglund@hotmail.com>
2018-11-15 22:48:26 +0200

    Rework class ShoppingListItem

--- src/main/java/com/github/anthogis/shoppinglist/ShoppingListItem.java
+++ src/main/java/com/github/anthogis/shoppinglist/ShoppingListItem.java
@@ -1,27 +1,34 @@
 package com.github.anthogis.shoppinglist;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class ShoppingListItem {
-    private String itemName;
-    private int itemAmount;
+    private final SimpleStringProperty itemName = new SimpleStringProperty("");
+    private final SimpleIntegerProperty itemAmount = new SimpleIntegerProperty(0);
 
     public ShoppingListItem(String itemName, int itemAmount) {
-        this.itemName = itemName;
-        this.itemAmount = itemAmount;
+        setItemName(itemName);
+        setItemAmount(itemAmount);
+    }
+
+    public ShoppingListItem() {
+        this("",0);
     }
 
     public String getItemName() {
-        return itemName;
+        return itemName.get();
     }
 
     public void setItemName(String itemName) {
-        this.itemName = itemName;
+        this.itemName.set(itemName);
     }
 
     public int getItemAmount() {
-        return itemAmount;
+        return itemAmount.get();
     }
 
     public void setItemAmount(int itemAmount) {
-        this.itemAmount = itemAmount;
+        this.itemAmount.set(itemAmount);
     }
 }
