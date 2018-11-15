a7dde9459a4442756ef48ded6284a3c69ca98a5c
antHogis <antonjhoglund@hotmail.com>
2018-11-15 14:41:23 +0200

    Create dummy implementation of JSONFormatter

--- src/main/java/com/github/anthogis/json_parser/JSONFormatter.java
+++ src/main/java/com/github/anthogis/json_parser/JSONFormatter.java
@@ -1,4 +1,22 @@
 package com.github.anthogis.json_parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JSONFormatter {
+    private String jsonData;
+    private List<String> jsonDataLines;
+
+    public JSONFormatter(String jsonData) {
+        this.jsonData = jsonData;
+        this.jsonDataLines = new ArrayList<>();
+    }
+
+    public void formatData() {
+        jsonDataLines.add(jsonData);
+    }
+
+    public List<String> getJsonDataLines() {
+        return jsonDataLines;
+    }
 }
