702bae24f6be9773ab89ce63652a57fdb139414b
antHogis <antonjhoglund@hotmail.com>
2018-11-16 14:59:47 +0200

    Add javadoc for App

--- src/main/java/com/github/anthogis/shoppinglist/App.java
+++ src/main/java/com/github/anthogis/shoppinglist/App.java
@@ -4,10 +4,21 @@ import com.github.anthogis.shoppinglist.gui.MainWindow;
 import javafx.application.Application;
 
 /**
- * Hello world!
+ * The main class of the application.
  *
+ * <p>The main class of the application. Launches MainWindow.</p>
+ *
+ * @author antHogis
+ * @version 1.0
+ * @since 1.0
  */
 public class App {
+
+    /**
+     * Launches MainWindow.
+     *
+     * @param args command line parameters, passed to javafx.application.Application.launch().
+     */
     public static void main( String[] args ) {
         Application.launch(MainWindow.class, args);
     }
