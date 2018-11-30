a204e98222600ccfb7419eb65528497f505d0f68
antHogis <antonjhoglund@hotmail.com>
2018-11-30 14:30:07 +0200

    Add constructors to ShoppingListMalformedException

--- ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/ShoppingListMalformedException.java
+++ ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/ShoppingListMalformedException.java
@@ -1,4 +1,11 @@
 package com.github.anthogis.shoppinglist;
 
 public class ShoppingListMalformedException extends RuntimeException {
+    public ShoppingListMalformedException(String message) {
+        super(message);
+    }
+
+    public ShoppingListMalformedException() {
+        super("Something in the shoppinglist file was malformed.");
+    }
 }
