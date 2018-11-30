5040639f1c3a5a474c90a1bc9bb1f1092f88c7a8
antHogis <antonjhoglund@hotmail.com>
2018-11-30 13:30:11 +0200

    Add constructor to ShoppingListReader

--- ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/ShoppingListReader.java
+++ ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/ShoppingListReader.java
@@ -1,4 +1,24 @@
 package com.github.anthogis.shoppinglist;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ShoppingListReader {
+    private List<ShoppingListItem> shoppingList;
+    private List<String> fileLines;
+
+    public ShoppingListReader(String filePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        fileLines = reader.lines().collect(Collectors.toList());
+        reader.close();
+        parseLines();
+    }
+
+    private void parseLines() {
+
+    }
 }
