8045a0f7e08f0eace06bba9d19952f01b8ce08cb
antHogis <antonjhoglund@hotmail.com>
2018-12-10 12:17:46 +0200

    Add query String creators to H2Interface

--- ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/H2Interface.java
+++ ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/H2Interface.java
@@ -1,4 +1,44 @@
 package com.github.anthogis.shoppinglist;
 
+import java.util.List;
+
 public class H2Interface {
+
+    private String createShoppingListTable(String tableName) {
+        StringBuilder commandBuilder = new StringBuilder();
+        commandBuilder.append("CREATE TABLE ")
+                .append(tableName)
+                .append('(')
+                .append("ID INT PRIMARY KEY")
+                .append(",ITEM_NAME VARCHAR(255)")
+                .append(",ITEM_AMOUNT INT)");
+
+        return commandBuilder.toString();
+    }
+
+    private String insertItems(List<ShoppingListItem> listItems, String tableName)
+            throws NumberFormatException {
+        StringBuilder commandBuilder = new StringBuilder();
+
+        for (int i = 0; i < listItems.size(); i++) {
+            String itemName = listItems.get(i).getItemName();
+            if (itemName.length() > 255) {
+                itemName = itemName.substring(0, 255);
+            }
+
+            int itemAmount = Integer.parseInt(listItems.get(i).getItemAmount());
+
+            commandBuilder.append("INSERT INTO ")
+                    .append(tableName)
+                    .append(" VALUES(")
+                    .append(i)
+                    .append(',')
+                    .append('\'' + itemName + '\'')
+                    .append(',')
+                    .append(itemAmount)
+                    .append(')');
+        }
+
+        return commandBuilder.toString();
+    }
 }
