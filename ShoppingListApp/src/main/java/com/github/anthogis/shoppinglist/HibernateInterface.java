6ccaef530457708b46caa9b0e242a76913195136
antHogis <antonjhoglund@hotmail.com>
2018-12-10 13:00:02 +0200

    Enable storing ShoppingListItems w/ Hibernate

--- ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/HibernateInterface.java
+++ ShoppingListApp/src/main/java/com/github/anthogis/shoppinglist/HibernateInterface.java
@@ -1,4 +1,34 @@
 package com.github.anthogis.shoppinglist;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 public class HibernateInterface {
+    private Configuration configuration;
+    private SessionFactory sessionFactory;
+
+    public HibernateInterface() {
+        configuration = new Configuration();
+        configuration.configure();
+        configuration.addAnnotatedClass(ShoppingListItem.class);
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public void addValues(List<ShoppingListItem> listItems) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        for(ShoppingListItem item : listItems) {
+            session.persist(item);
+        }
+
+        transaction.commit();
+        session.close();
+    }
+
 }
