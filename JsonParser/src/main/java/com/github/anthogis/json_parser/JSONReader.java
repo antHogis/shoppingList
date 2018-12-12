0b722026a6c259262fd7099896044e9b45fde631
antHogis <antonjhoglund@hotmail.com>
2018-12-12 23:24:29 +0200

    Update JSONReader to check token validity

--- JsonParser/src/main/java/com/github/anthogis/json_parser/JSONReader.java
+++ JsonParser/src/main/java/com/github/anthogis/json_parser/JSONReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class JSONReader {
@@ -24,17 +25,46 @@ public class JSONReader {
 
     private JSONObject parseObject() throws JSONParseException {
         JSONObject object = null;
-        JSONToken current;
+        List<JSONToken> expectedTokens = new ArrayList();
+        expectedTokens.add(JSONToken.OBJECT_BEGIN);
 
         JSONTokenizer tokenizer = new JSONTokenizer(jsonLines);
-
 
         tokenizer.tokenize();
 
         for (Pair<JSONToken, String> pair : tokenizer.getTokens()) {
+            if (!tokenIsExpected(pair.getKey(), expectedTokens)) {
+                throw new JSONParseException();
+            }
+
+
 
         }
 
         return object;
+    }
+
+    private boolean tokenIsExpected(JSONToken actualToken, List<JSONToken> expectedTokens) {
+        boolean syntaxIsValid = false;
+
+        for (JSONToken expectedToken : expectedTokens) {
+            if (actualToken == expectedToken) {
+                syntaxIsValid = true;
+            }
+        }
+
+        return syntaxIsValid;
+    }
+
+    private List<JSONToken> expectValueList() {
+        return Arrays.asList(JSONToken.OBJECT_BEGIN,
+                JSONToken.NULL,
+                JSONToken.BOOLEAN,
+                JSONToken.NUMBER,
+                JSONToken.STRING);
+    }
+
+    private List<JSONToken> expectAssignList() {
+        return Arrays.asList(JSONToken.ASSIGN);
     }
 }
