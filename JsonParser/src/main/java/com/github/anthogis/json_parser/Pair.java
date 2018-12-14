f538db178d2dc593f3318815d5effb92613b9d00
antHogis <antonjhoglund@hotmail.com>
2018-12-14 13:43:51 +0200

    Add javadoc to Pair

--- JsonParser/src/main/java/com/github/anthogis/json_parser/Pair.java
+++ JsonParser/src/main/java/com/github/anthogis/json_parser/Pair.java
@@ -1,23 +1,41 @@
 package com.github.anthogis.json_parser;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * An object that holds two values.
+ *
+ * @param <K> the first value.
+ * @param <V> the second value.
+ */
 public class Pair<K, V> {
     private K key;
     private V value;
 
-
+    /**
+     * Constructor of a pair of two values.
+     *
+     * @param key the first value.
+     * @param value the second value.
+     */
     public Pair(K key, V value) {
         this.value = value;
         this.key = key;
     }
 
-    public V getValue() {
-        return value;
-    }
-
+    /**
+     * Returns the first value.
+     *
+     * @return the first value.
+     */
     public K getKey() {
         return key;
+    }
+
+    /**
+     * Returns the second value.
+     *
+     * @return the second value
+     */
+    public V getValue() {
+        return value;
     }
 }
