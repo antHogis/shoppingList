5f5d48363bd04f48a7a4f207c20d6689f2cc1ddb
antHogis <antonjhoglund@hotmail.com>
2018-12-14 13:49:53 +0200

    Add javadoc to JSONParseException

--- JsonParser/src/main/java/com/github/anthogis/json_parser/JSONParseException.java
+++ JsonParser/src/main/java/com/github/anthogis/json_parser/JSONParseException.java
@@ -1,14 +1,25 @@
 package com.github.anthogis.json_parser;
 
+/**
+ * An exception thrown if there's a syntax error in a parsed JSON file.
+ */
 public class JSONParseException extends  RuntimeException {
-    public JSONParseException() {
-        super("Error in parsing JSON");
-    }
 
+    /**
+     * Constructs a JSONParseException with a custom message.
+     *
+     * @param message the custom message.
+     */
     public JSONParseException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a JSONParseException that informs on which row and column of a JSON file the exception was encountered.
+     *
+     * @param row the row where the exception was encountered.
+     * @param col the column where the exception was encountered.
+     */
     public JSONParseException(int row, int col) {
         super(String.format("Illegal character in json file at [%d, %d]", row, col));
     }
