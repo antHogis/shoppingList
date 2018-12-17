7d34da394b8d75105e170dce42c6902abc8d909d
antHogis <antonjhoglund@hotmail.com>
2018-12-18 01:03:18 +0200

    Add javadoc for JSONToken

--- JsonParser/src/main/java/com/github/anthogis/json_parser/JSONToken.java
+++ JsonParser/src/main/java/com/github/anthogis/json_parser/JSONToken.java
@@ -1,22 +1,67 @@
 package com.github.anthogis.json_parser;
 
 /**
- * TODO Write javadoc
+ * Tokens for parsing JSON.
+ *
+ * Contains tokens that a {@link JSONParser} can parse.
  *
  * @author antHogis
  * @version 1.3
  * @since 1.2
  */
 public enum JSONToken {
+    /**
+     * Token for when an object begins.
+     */
     OBJECT_BEGIN,
+
+    /**
+     * Token for when an object ends.
+     */
     OBJECT_END,
+
+    /**
+     * Token for when an array begins.
+     */
     ARRAY_BEGIN,
+
+    /**
+     * Token for when an array ends.
+     */
     ARRAY_END,
+
+    /**
+     * Token for when an assign symbol is encountered.
+     */
     ASSIGN,
+
+    /**
+     * Token for a key to a value.
+     */
     KEY,
+
+    /**
+     * Token for a delimiter between values.
+     */
     DELIMITER,
+
+    /**
+     * Token for a string value.
+     */
     STRING,
+
+    /**
+     * Token for an integer value.
+     */
     INTEGER,
+
+    /**
+     * Token for a boolean value.
+     */
     BOOLEAN,
+
+    /**
+     * Token for a null value.
+     */
     NULL
 }
