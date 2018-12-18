893e823dc07f80085a8dbc0950a33a9dd15155c7
antHogis <antonjhoglund@hotmail.com>
2018-12-18 15:59:02 +0200

    Update tests for JSONObject

--- JsonParser/src/test/java/com/github/anthogis/json_parser/JSONObjectTest.java
+++ JsonParser/src/test/java/com/github/anthogis/json_parser/JSONObjectTest.java
@@ -2,54 +2,95 @@ package com.github.anthogis.json_parser;
 
 import com.github.anthogis.json_parser.api.JSONAttribute;
 import com.github.anthogis.json_parser.api.JSONObject;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.AbstractList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
 public class JSONObjectTest {
-    static JSONObject testObjectOne;
-    static JSONObject testObjectTwo;
+    static JSONObject object;
+    static StringBuilder expected;
 
-    @BeforeClass
-    public static void init() {
-        testObjectTwo = new JSONObject();
-        testObjectTwo.getValues().add(new JSONAttribute<Integer>("fsds", 12));
-        testObjectTwo.getValues().add(new JSONAttribute<Boolean>("boolean", true));
-        ArrayList<String> al = new ArrayList<>();
-        al.add("derp1");
-        al.add("derp2");
-        al.add("derp3");
-        ArrayList<Integer> al2 = new ArrayList<>();
-        testObjectTwo.getValues().add(new JSONAttribute<List>("list", al));
-        testObjectTwo.getValues().add(new JSONAttribute<List>("list 2", al2));
-        testObjectTwo.getValues().add(new JSONAttribute<String>("strink", null));
+    private void runTest() {
+        expected.append('}');
+        object.formatObject();
+        System.out.println("Expected : " + expected.toString());
+        System.out.println("Actual   : " + object.getNotation());
+        assertEquals(expected.toString(), object.getNotation());
+    }
 
-        testObjectOne = new JSONObject();
-        testObjectOne.getValues().add(new JSONAttribute<Integer>("fsds", 12));
-        testObjectOne.getValues().add(new JSONAttribute<Boolean>("boolean", true));
-        testObjectOne.getValues().add(new JSONAttribute<List>("list", al));
-        testObjectOne.getValues().add(new JSONAttribute<List>("list 2", al2));
-        testObjectOne.getValues().add(new JSONAttribute<String>("strink", null));
-        testObjectOne.getValues().add(new JSONAttribute<JSONObject>("object", testObjectTwo));
-        testObjectOne.formatObject();
+    @Before
+    public void initObject() {
+        object = new JSONObject();
+        expected = new StringBuilder().append('{');
     }
 
     @Test
-    public void testNotation() {
-        System.out.println(testObjectOne.getNotation());
-        //assertTrue(testObjectOne.getNotation().equals("{\"fsds\" : 12,\"very gay\" : true}"));
+    public void testAddAndGet() {
+        JSONAttribute attribute = new JSONAttribute<>("blaze it", 420);
+        object.add(attribute);
+        assertEquals(1, object.getValues().size());
+        assertEquals(attribute, object.getValues().get(0));
     }
 
+    @Test
+    public void testEmpty() {
+        runTest();
+    }
 
     @Test
-    public void testWriter() throws IOException {
-        //JSONWriter jsonWriter = new JSONWriter(testObjectOne, "test");
-        //assertTrue(jsonWriter.writeFile());
+    public void testWithSingleValue() {
+        object.add(new JSONAttribute<>("int", 234));
+        expected.append('"').append("int").append("\" : ").append(234);
+
+        runTest();
+    }
+
+    @Test
+    public void testWithMultipleValues() {
+        object.add(new JSONAttribute<>("string", "skrrt dab on 'em haters"));
+        expected.append('"').append("string").append("\" : ").append("\"skrrt dab on 'em haters\"");
+
+        object.add(new JSONAttribute<>("bool", false));
+        expected.append(",\"").append("bool").append("\" : ").append("false");
+
+        object.add(new JSONAttribute<>("int", -234));
+        expected.append(',').append('"').append("int").append("\" : ").append(-234);
+
+        runTest();
+    }
+
+    @Test
+    public void testNestedObject() {
+        JSONObject nested = new JSONObject();
+        nested.add(new JSONAttribute<>("nested nested", new JSONObject()));
+        nested.add(new JSONAttribute<>("key", "value"));
+        object.add(new JSONAttribute<>("nested", nested));
+
+        expected.append("\"nested\" : ").append('{').append("\"nested nested\" : {}")
+                .append(',').append("\"key\" : \"value\"").append('}');
+
+        runTest();
+    }
+
+    @Test
+    public void testCollection() {
+        JSONObject nested = new JSONObject();
+        nested.add(new JSONAttribute<>("nested nested", new JSONObject()));
+        nested.add(new JSONAttribute<>("int", 420));
+
+        object.add(new JSONAttribute<>("array",
+                Arrays.asList("fsdfafd", 233, true, null, nested)));
+        expected.append("\"array\" : [").append("\"fsdfafd\"").append(',').append(233).append(',').append("true")
+                .append(',').append("null").append(',').append("{\"nested nested\" : {},")
+                .append("\"int\" : ").append(420).append('}').append(']');
+
+        runTest();
     }
 }
